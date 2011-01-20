7f59480f605c280ca6860083de1cc39b3af9d398
Nick Stenning <nick@whiteink.com>
2011-01-20 11:42:22 +0000

    Add Unicycle class

--- MawWorld.cpp
+++ MawWorld.cpp
@@ -33,7 +33,6 @@ void MawWorld::initPhysics() {
   m_dynamicsWorld->setGravity(btVector3(0, 0, -10));
 
   // Create ground
-  //btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0,0,1), 0);
   btCollisionShape* groundShape = new btBoxShape(btVector3(10,10,1));
   m_collisionShapes.push_back(groundShape);
 
@@ -43,10 +42,7 @@ void MawWorld::initPhysics() {
 
   localCreateRigidBody(0.f, groundTransform, groundShape);
 
-  addUnicycle(btVector3( 0,  0, 0.8));
-  addUnicycle(btVector3( 1,  1, 0.8));
-  addUnicycle(btVector3(-1,  1, 0.8));
-  addUnicycle(btVector3( 1, -1, 0.8));
+  addUnicycle(btVector3(0, 0, 0.8));
 }
 
 void MawWorld::stepSimulation(float dt) {
@@ -91,72 +87,6 @@ void MawWorld::endPhysics() {
   delete m_collisionConfiguration;
 }
 
-void MawWorld::addUnicycle(btVector3 const& pos) {
-
-  float fork[]  = {0.05, 0.8}; // box width, length
-  float wheel[] = {0.1, 0.4}; // width, radius
-
-  // Make the unicycle fork
-
-  btCompoundShape* forkShape = new btCompoundShape();
-
-  {
-    float totalForkWidth = 2 * (fork[0] + wheel[0]);
-    btCollisionShape* forkLeg = new btBoxShape(btVector3(fork[0]/2, fork[0]/2, fork[1]/2));
-    btCollisionShape* forkTop = new btBoxShape(btVector3(totalForkWidth/2, fork[0]/2, fork[0]/2));
-    btCollisionShape* seat = new btCapsuleShape(totalForkWidth * 0.5, fork[1] * 0.2);
-
-    m_collisionShapes.push_back(forkLeg);
-    m_collisionShapes.push_back(forkTop);
-    m_collisionShapes.push_back(seat);
-
-    btTransform forkLegATrans, forkLegBTrans, forkTopTrans, seatTrans;
-    forkLegATrans.setIdentity();
-    forkLegBTrans.setIdentity();
-    forkTopTrans.setIdentity();
-    seatTrans.setIdentity();
-
-    forkLegATrans.setOrigin(btVector3(-(totalForkWidth/2 - fork[0]/2), 0, 0));
-    forkLegBTrans.setOrigin(btVector3( (totalForkWidth/2 - fork[0]/2), 0, 0));
-    forkTopTrans.setOrigin(btVector3(0, 0, fork[1]/2 - fork[0]/2));
-    seatTrans.setOrigin(btVector3(0, 0, (fork[1] * 1.2)/2));
-
-    forkShape->addChildShape(forkLegATrans, forkLeg);
-    forkShape->addChildShape(forkLegBTrans, forkLeg);
-    forkShape->addChildShape(forkTopTrans, forkTop);
-    forkShape->addChildShape(seatTrans, seat);
-  }
-
-  // Make the unicycle wheel
-
-  btCollisionShape* wheelShape = new btCylinderShapeX(btVector3(wheel[0]/2, wheel[1], wheel[1]));
-
-  m_collisionShapes.push_back(forkShape);
-  m_collisionShapes.push_back(wheelShape);
-
-  btTransform forkTrans, wheelTrans;
-  forkTrans.setIdentity();
-  wheelTrans.setIdentity();
-
-  forkTrans.setOrigin(pos);
-  wheelTrans.setOrigin(pos + btVector3(0, 0, -fork[1]/2));
-
-  btRigidBody* forkBody  = localCreateRigidBody(0, forkTrans, forkShape);
-  btRigidBody* wheelBody = localCreateRigidBody(0.5, wheelTrans, wheelShape);
-
-  btVector3 pivotInWheel1(wheel[0], 0, 0);
-  btVector3 pivotInFork1(wheel[0], 0, -fork[1]/2);
-  btVector3 pivotInWheel2(-wheel[0], 0, 0);
-  btVector3 pivotInFork2(-wheel[0], 0, -fork[1]/2);
-  btVector3 axisInWheel(1, 0, 0);
-  btVector3 axisInFork(1, 0, 0);
-
-  btTypedConstraint* hinge1 = new btHingeConstraint(*wheelBody, *forkBody, pivotInWheel1, pivotInFork1, axisInWheel, axisInFork);
-  btTypedConstraint* hinge2 = new btHingeConstraint(*wheelBody, *forkBody, pivotInWheel1, pivotInFork1, axisInWheel, axisInFork);
-
-  m_dynamicsWorld->addConstraint(hinge1);
-  m_dynamicsWorld->addConstraint(hinge2);
-}
 
 void MawWorld::clientMoveAndDisplay() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
---
+++ Unicycle.h
@@ -0,0 +1,26 @@
+/*
+ *  Unicycle.h
+ *  maw
+ *
+ *  Created by Nick Stenning on 14/12/2010.
+ *  Copyright 2010 Nick Stenning. All rights reserved.
+ *
+ */
+
+#include <BulletDynamics/btBulletDynamicsCommon.h>
+
+class Unicycle {
+public:
+  Unicycle () {}
+  ~Unicycle () {}
+
+  Unicycle& position(btVector3 const& pos);
+  btVector3& position() const;
+
+  static const btScalar forkWidth;
+  static const btScalar forkLength;
+  static const btScalar wheelWidth;
+  static const btScalar wheelRadius;
+private:
+  btVector3 const& m_position;
+}
